f4ccaaf47f54a9c097faae51345bb396b01edb0c
icanttakeitanymore <polozovbv@gmail.com>
2016-04-08 11:44:12 +0400

    Create dict.py

---
+++ study/python/dict.py
@@ -0,0 +1,5 @@
+for line in open('/proc/vmstat').readlines():                     
+    if 'pgscan' in line:
+    l = line.rstrip().split(" ")
+        for (key,value) in l:
+            d[key] = value
